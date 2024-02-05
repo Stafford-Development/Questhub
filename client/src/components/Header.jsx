e69229bacccd7e09f7f1fa92878646014878ddd8
Ethan Stafford <ethanst@colostate.edu>
2024-02-05 08:36:16 -0800

    client skeleton

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-
+import ProfilePopup from './profilePopup.jsx';
+import "../App.css"
+const profilePicUrl = 'https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/327181129_747596569830920_5108350515149982305_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=GNQAo5rc-jAAX_J2NzV&_nc_ht=scontent-lax3-1.xx&oh=00_AfDfpWFtBdK1l6mUYhsCSVZ_m1XCDG5_FCCtvEFRAZS1hQ&oe=65C3AF5C';
 function Header() {
   
     return (
@@ -16,6 +18,9 @@ function Header() {
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
+            <Nav className='ml-auto'>
+              <ProfilePopup profilePicUrl={profilePicUrl} />
+            </Nav>
           </Nav>
         </Navbar>
     )
