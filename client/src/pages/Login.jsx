2deed0f88be15be896d7928df902199e593d24ac
Ethan Stafford <ethanst@colostate.edu>
2024-02-06 15:03:16 -0800

    changed background

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,10 +7,10 @@ import {Button, Form, Image, Container, Card, Row, Col } from 'react-bootstrap';
 function Login(isLoggedin) {
   return (
     <Container fluid>
-      <Image className="login-background" src="https://assetstorev1-prd-cdn.unity3d.com/key-image/770a4cc3-43ec-4ff8-b36c-036612454a40.png"/>
+      <Image className="login-background" src="/Town.gif"/>
       <Row className='g-0'>
         <Col>
-          <Card className='bg-dark text-white h-100' >
+          <Card className='bg-dark text-white h-100 logo-box' >
             <Card.Body className="d-flex align-items-center justify-content-center">
               <h1 className='login-name'>Quester</h1>
               <p>Dungeons. Danger. Quests.</p>
