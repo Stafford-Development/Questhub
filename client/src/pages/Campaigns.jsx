02c7ad76e6a37261e488adc75abbd5a7aada6f59
Ethan Stafford <ethanst@colostate.edu>
2024-02-12 20:38:29 -0800

    Campaign Blank Page

--- client/src/pages/Campaigns.jsx
+++ client/src/pages/Campaigns.jsx
@@ -1,18 +1,13 @@
 import react from 'react';
 import CampaignCarousel from '../components/CampaignCarousel';
 import {Container, Card, Image} from 'react-bootstrap';
+import '../styling/App.css';
 
 function Campaigns() {
     return (
         <Container fluid>
             <Image className="login-background" src="/Town.gif"/>  
-            <Card className='bg-light text-black'>
-                <Card.Title as="h2">Campaigns</Card.Title>
-                <Card.Body>
-                    <CampaignCarousel/>
-                </Card.Body>
-            </Card>
-
+            <Container fluid className="login-container"> </Container>
         </Container>
     );
 };
