8ad1ab5df3d560f5bab6c64b525a4ba999833960
Ethan Stafford <ethanst@colostate.edu>
2024-04-27 00:09:11 -0700

    added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [
+      {
+        name: 'server',
+        script: './server/server.js',
+        env: {
+          NODE_ENV: 'development',
+        },
+        env_production: {
+          NODE_ENV: 'production',
+        },
+      },
+      {
+        name: 'client',
+        script: 'serve',
+        env: {
+          PM2_SERVE_PATH: './client',
+          PM2_SERVE_PORT: 8080,
+        },
+        env_production: {
+          PM2_SERVE_PATH: './client',
+          PM2_SERVE_PORT: 80,
+        },
+      },
+    ],
+  };
