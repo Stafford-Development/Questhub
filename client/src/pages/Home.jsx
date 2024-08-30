dd54a2576195ef237c5f4283470374cff24ecf5c
Ethan Stafford <ethanstafford.dev@gmail.com>
2024-08-30 15:11:08 -0700

    meow

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import '../styling/App.css'
 import {Button, Form } from 'react-bootstrap';
-import SelectCampaignCarousel from '../components/SelectCampaignCarousel';
+
 
 
 
